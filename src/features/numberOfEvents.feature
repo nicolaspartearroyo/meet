3acfbe6a649bdead8610f52ef1e24ca2bd9e662d
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-08-08 19:47:14 +0200

    numberOfEventes test on features fixed

--- src/features/numberOfEvents.feature
+++ src/features/numberOfEvents.feature
@@ -3,7 +3,7 @@ Feature: Specify number of events
 Scenario: When user hasn’t specified a number, 32 is the default number 
 Given user open the app
 When user look for an event
-Then thirty two events max will be shown
+Then thirty two events will be shown
 
 Scenario: User can change the number of events they want to see 
 Given user open the app
--- src/features/numberOfEvents.test.js
+++ src/features/numberOfEvents.test.js
@@ -1,16 +1,14 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from '../App';
-import Event from '../Event';
 import NumberOfEvents from "../NumberOfEvents";
 import { mockData } from '../mock-Data';
 
 const feature = loadFeature('./src/features/numberOfEvents.feature');
 
 defineFeature(feature, test => {
-  let AppWrapper;
-  let NumberOfEventWrapper;
+  const NumberOfEventsWrapper = mount(<NumberOfEvents />);
 
   test('When user hasn’t specified a number, 32 is the default number', ({ given, when, then }) => {
     given('user open the app', () => {
@@ -20,8 +18,8 @@ defineFeature(feature, test => {
     when('user look for an event', () => {
     });
 
-    then('thirty two events max will be shown', () => {
-      expect(AppWrapper.state('numberOfEvents').length).toBe(mockData.length);
+    then('thirty two events will be shown', () => {
+      expect(NumberOfEventsWrapper.state('numberOfEvents').lenght).toBe(32);
     });
   });
 
@@ -32,11 +30,10 @@ defineFeature(feature, test => {
 
     when('user change the number of events', () => {
     });
-    NumberOfEventWrapper.find('numberOfEvents').simulate('change', { target: { value: '1' } });
+    NumberOfEventsWrapper.find('.eventNum').simulate('change', { target: { value: 1 } });
 
     then('number of events will change', () => {
-      AppWrapper.update();
-      expect(AppWrapper.state('events').length).toBe(1);
+      expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(1);
     });
   });
 
