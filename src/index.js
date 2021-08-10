89e4213e423d8d12e3a46cf9a48b5375f08440a1
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-08-10 18:01:47 +0200

    atatus notify error

--- src/index.js
+++ src/index.js
@@ -23,3 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
