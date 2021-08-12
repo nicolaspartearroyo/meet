f42a55908ca1ea51f9e943c6cac0cb9f6b8d9818
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-08-12 18:52:40 +0200

    privacy.html added

--- src/api.js
+++ src/api.js
@@ -69,7 +69,11 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
-
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(events).events : [];;
+  }
   const token = await getAccessToken();
 
   if (token) {
