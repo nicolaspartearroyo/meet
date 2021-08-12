7bc70699702f79f9d8d9965db26e7bc794f6cc08
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-08-12 12:41:25 +0200

    atatus disable

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -10,7 +10,6 @@ class NumberOfEvents extends Component {
   handleInputChanged = (event) => {
     const value = event.target.value;
     this.props.updateEventCount(event.target.value);
-
     if (value.length <= 0) {
       this.setState({
         infoText: 'Select a number from 1 to 32.',
