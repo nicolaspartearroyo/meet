4390b9ca0edce98badf414a5032f67be3f3a4215
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-08-13 07:01:18 +0200

    atatus out

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
-atatus.config('61839cf67d3b4e7e8c1ddf4ff880eef0').install();
+// import * as atatus from 'atatus-spa';
+// atatus.config('61839cf67d3b4e7e8c1ddf4ff880eef0').install();
 
 ReactDOM.render(
   <React.StrictMode>
