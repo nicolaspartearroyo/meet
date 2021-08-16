aa013cb861b5325cbd7f34acbe33861bceb95804
nicolaspartearroyo <nicolaspartearroyo@gmail.com>
2021-08-16 14:17:32 +0200

    handleInputChanged on Number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -9,11 +9,23 @@ class NumberOfEvents extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({
-      numberOfEvents: value,
-    });
-    this.props.updateEvents(event.target.value);
-
+    if (value < 1) {
+      return this.setState({
+        numberOfEvents: '',
+        infoText: 'Please choose a number between 1 and 32',
+      });
+    } else if (value > 32) {
+      return this.setState({
+        numberOfEvents: '',
+        infoText: 'Please choose a number smaller than 32',
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        infoText: '',
+      });
+      this.props.updateEvents('', value);
+    }
   };
 
   render() {
